59ae1c00dd62c75e59cee6ad7dd5d1b3cfe537ed
Philip Raschke <philip.raschke@tu-berlin.de>
2022-03-16 13:18:51 +0100

    implemented preliminary Labeling page

--- src/components/content/Blocklists.vue
+++ src/components/content/Blocklists.vue
@@ -1,149 +1,62 @@
 <template>
   <div class="row">
     <div class="col">
-      <b>Blocklists</b>
+      <b>Labeling</b>
     </div>
-  </div>  
+  </div>
+  <div class="row">
+    <div class="col">
+      <table class="table table-hover align-middle mt-3">
+        <thead>
+          <th
+            v-for="heading, index in Object.keys(lists[0])"
+            :key="index" 
+            scope="col"
+          >
+            {{ heading.charAt(0).toUpperCase() + heading.slice(1) }}
+          </th>
+        </thead>
+        <tbody>
+          <tr 
+            v-for="list, index in lists"
+            :key="index"
+            scope="row"
+          >
+            <td>{{ list.name }}</td>
+            <td>{{ list.version }}</td>
+            <td>
+              <a 
+                :href="list.url"
+                target="_blank"
+              >
+                {{ list.url }}
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import Util from "../../model/Util.js";
-
 export default {
-  components: {
-  },
-  props: {
-    http: {
-      type: Array,
-      default: () => []
-    },
-    tag: {
-      type: String,
-      default: () => ""
-    }
-  },
   data: () => {
     return {
-      loading: {
-        isLoading: false,
-        current: 0,
-        max: 1,
-      },
-      counts: {},
-      countsItems: [],
-      rules: {},
-      rulesItems: [],
-      types: {},
-      typesItems: [],
-      currentRulesPage: [],
-      currentTypesPage: [],
-      perPage: 5,
-      numOfRequests: 0,
+      lists: [{
+        name: "EasyList",
+        version: "latest",
+        url: "https://easylist.to/easylist/easylist.txt"
+      }, {
+        name: "EasyPrivacy",
+        version: "latest",
+        url: "https://easylist.to/easylist/easyprivacy.txt"
+      }, {
+        name: "Disconnect.me",
+        version: "latest",
+        url: "https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/services.json"
+      }]
     }
-  },
-  mounted() {
-    /*
-    this.loading.isLoading = true;
-    Util.labeledStream(this.http, (chunk, current, total) => {
-      this.numOfRequests = total;
-      if (current === total) {
-        this.loading = {isLoading: false, current: 0, max: 1};
-      } else {
-        this.loading.current = current;
-        this.loading.max = total;
-      }
-      (chunk) ? this.processChunk(chunk) : null;
-    });
-    */
-  },
-  methods: {
-    processChunk: function(chunk) {
-      chunk
-        .forEach((r, index) => 
-          r.labels
-            .forEach((label, i) => {
-              if (label.isLabeled) {
-                (this.counts[label.blocklist]) ? 
-                  this.counts[label.blocklist]++ 
-                  : this.counts[label.blocklist] = 1;             
-              }
-
-              (!this.rules[label.blocklist]) ? this.rules[label.blocklist] = {} : null;
-              (label.rule) ?
-                (this.rules[label.blocklist][label.rule]) ? 
-                  this.rules[label.blocklist][label.rule]++ 
-                  : this.rules[label.blocklist][label.rule] = 1
-                : null;
-              
-              (!this.types[label.blocklist]) ? this.types[label.blocklist] = {} : null;  
-              (label.type) ?
-                (typeof label.type === "object") ?
-                  label.type.forEach((type) => {
-                    (this.types[label.blocklist][type]) ?
-                      this.types[label.blocklist][type]++ 
-                      : this.types[label.blocklist][type] = 1                    
-                  })
-                  : (this.types[label.blocklist][label.type]) ?
-                    this.types[label.blocklist][label.type]++ 
-                    : this.types[label.blocklist][label.type] = 1
-                : null;
-
-              if (index === chunk.length-1 && 
-                  i === r.labels.length-1) {
-                    this.countsItems = [this.counts, this.toPercent()];
-                    this.rulesItems = this.toTable(this.rules);
-                    this.typesItems = this.toTable(this.types);
-                  }
-            })
-          )
-    },
-    toPercent: function() {
-      let result = {}
-      for (let key of Object.keys(this.counts)) {
-        result[key] = ((this.counts[key] / this.numOfRequests) * 100).toFixed(2);
-      }
-      return result;
-    },
-    toTable: function(map) {
-      return Object
-        .keys(map)
-        .map((list) => 
-          Object.entries(map[list])
-            .map((pair) => 
-              ({
-                value: pair[0], 
-                count: pair[1], 
-                percent: ((pair[1] / this.counts[list]) * 100).toFixed(2),
-                ofTotal: ((pair[1] / this.numOfRequests) * 100).toFixed(2),
-              })
-            )
-          );
-    },
-    exportData(list, label, table) {
-      table.unshift(
-        Object.keys(table[0])
-          .reduce((acc, val) => 
-            (acc[val] = val, acc), {})
-        );
-      let csv = table
-        .map((row) => 
-          Object.values(row)
-            .map((value) => 
-              '"' + value + '"'
-            ).join(",")
-          ).join("\r\n");
-      csv = '"N=",' + this.numOfRequests + "\r\n" + csv;
-      chrome.downloads.download({
-        filename: 
-          this.tag
-          + "-"
-          + list 
-          + "-" 
-          + label 
-          + ".csv",
-        url: URL.createObjectURL(new Blob([csv], {type: "data:application/csv;charset=utf-8"})),
-      });
-    },    
-  },
+  }
 }
 </script>
