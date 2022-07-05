96d2ec2f3d1eaf2331aa3bcefcb1042149e1af3c
Philip Raschke <philip.raschke@tu-berlin.de>
2022-07-05 12:54:39 +0200

    fixed wrong and unneccessary uninitialized check in line 41, which caused a bug, when a different feature for the same group was queried; implemented StatisticsTmpStorage to mitigate OutOfMemory exceptions in Chromium-based browsers

--- src/model/Statistics.js
+++ src/model/Statistics.js
@@ -1,28 +1,32 @@
 import FeatureExtractor from "./FeatureExtractor.js";
 import Data from "./Data.js";
+import StatisticsTmpStorage from "./StatisticsTmpStorage.js";
 
 var Statistics = (() => {
   let stats = {};
 
   return {
+    /*
+     * @param {Array} c - Filtered chunk.
+     * @param {String} feature - The identifier of the feature.
+     * @param {String} id - The identifier of the query.
+     * @param {Number} gi - The index of the group in the members array.
+     * @param {Function} fn - The callback of the consuming component.
+     */
     count: (c, feature, id, gi, fn) => {
       c.forEach((d, i, array) => {
         let x = FeatureExtractor.extract(feature, d);
         if (typeof x === "object") {
           x.forEach((e, j, arr) => {
             let kv = FeatureExtractor.encode(e);
-            (stats[id][gi].data[feature][kv])
-              ? stats[id][gi].data[feature][kv]++
-              : stats[id][gi].data[feature][kv] = 1;
+            stats[id][gi].data[feature].put(kv);
 
             if (j === arr.length - 1) {
               fn(stats[id]);
             }
           });
         } else {
-          (stats[id][gi].data[feature][x])
-            ? stats[id][gi].data[feature][x]++
-            : stats[id][gi].data[feature][x] = 1;
+          stats[id][gi].data[feature].put(x);
 
           if (i === array.length - 1) {
             fn(stats[id]);
@@ -30,18 +34,33 @@ var Statistics = (() => {
         }
       });
     },
+    // TODO: or queries, features -> callback with id
     query: function (type, query, feature, callback) {
 
-      stats[query.id] = {};
+      if (!stats[query.id]) {
+        stats[query.id] = {};
+      }
+
+      // NOTE: In current implementation sufficient to check
+      //       if feature was computed for 0-th group member.
+      if (stats[query.id][0] &&
+        stats[query.id][0].data &&
+        stats[query.id][0].data[feature]) {
+        callback(stats[query.id]);
+        return;
+      }
 
       Data.stream(type, (chunk, index, total) => {
         if (chunk) {
-          query.members.forEach((g, i, arr) => {
+          query.members.forEach((g, i) => {
             let c = chunk.filter(g.filter);
-            if (!stats[query.id][i]) {
-              stats[query.id][i] = { count: 0, data: { [feature]: {} } };
+
+            stats[query.id][i] = {
+              data: {
+                [feature]: new StatisticsTmpStorage()
+              }
+
             }
-            stats[query.id][i].count += c.length;
             this.count(c, feature, query.id, i, callback);
           });
         }
@@ -54,7 +73,8 @@ var Statistics = (() => {
         }));
       });
     },
-    sum: function(data) {
+    // TODO: queryAll maybe?
+    sum: function (data) {
       return data.reduce((acc, val) => acc += val, 0);
     }
   };
