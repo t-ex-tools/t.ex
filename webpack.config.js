0b011cfbacaf378139a1360bed3d79124320c603
Philip Raschke <philip.raschke@tu-berlin.de>
2022-08-02 15:26:17 +0200

    updated dependencies; updated t.ex-tension; changed webpack.config.js to mode=development, need to parameterize config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ module.exports = (env) => {
 
   let config = {
     entry: "./index.js",
-    mode: "production",
+    mode: "development",
+    devtool: 'inline-source-map',
+    optimization: {
+      minimize: false
+    },
     output: {
       filename: "index.js",
       path: path.resolve(__dirname, "dist"),
